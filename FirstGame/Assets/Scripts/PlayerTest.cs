0794abbaaa49715755ea2d4abf9690efc5709ee5
Stealthairs <ogund009@umn.edu>
2022-11-21 18:15:33 -0600

    Update PlayerTest.cs
    Quick updates to a new class to learn move functionalities better

--- FirstGame/Assets/Scripts/PlayerTest.cs
+++ FirstGame/Assets/Scripts/PlayerTest.cs
@@ -5,14 +5,49 @@ using UnityEngine;
 public class PlayerTest : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    // Player movement tutorial
+
+    //Vector2: (X,Y)
+    //Vector3: (X,Y,Z)
+
+    //Physical body
+
+    [SerializeField]
+    private float speed;
+
+    private Rigidbody2D body;
+
+    float horizontalMove = 0f;
+
+    float vertMove = 0f;
+
+    [SerializeField]
+    float jumpForce = 0f;
+
+
+    private void Awake()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
+
+
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+      
+
+        body.velocity = new Vector2(horizontalMove * speed, body.velocity.y);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            body.velocity = new Vector2(body.velocity.x,jumpForce);
+        }
+
+        Debug.Log(body.velocity);
     }
+
+
 }
